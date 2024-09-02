53d2e36961fb7cf00d5ff0eaa6be051dc01cdb71
fabiokusaba <fabinhokb44@gmail.com>
2024-09-01 21:38:53 -0300

    Initial commit

---
+++ src/test/kotlin/com/fabiokusaba/bookstore/BookstoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fabiokusaba.bookstore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
