a8121414e0f789218eef0ff883c8d06b93ea8e0a
fabiokusaba <fabinhokb44@gmail.com>
2024-09-05 21:06:02 -0300

    Implemented save method in service layer

---
+++ src/main/kotlin/com/fabiokusaba/bookstore/services/AuthorService.kt
@@ -0,0 +1,7 @@
+package com.fabiokusaba.bookstore.services
+
+import com.fabiokusaba.bookstore.domain.entities.AuthorEntity
+
+interface AuthorService {
+    fun save(authorEntity: AuthorEntity) : AuthorEntity
+}
---
+++ src/main/kotlin/com/fabiokusaba/bookstore/services/impl/AuthorServiceImpl.kt
@@ -0,0 +1,14 @@
+package com.fabiokusaba.bookstore.services.impl
+
+import com.fabiokusaba.bookstore.domain.entities.AuthorEntity
+import com.fabiokusaba.bookstore.repositories.AuthorRepository
+import com.fabiokusaba.bookstore.services.AuthorService
+import org.springframework.stereotype.Service
+
+@Service
+class AuthorServiceImpl(private val authorRepository: AuthorRepository) : AuthorService {
+
+    override fun save(authorEntity: AuthorEntity): AuthorEntity {
+        return authorRepository.save(authorEntity)
+    }
+}
