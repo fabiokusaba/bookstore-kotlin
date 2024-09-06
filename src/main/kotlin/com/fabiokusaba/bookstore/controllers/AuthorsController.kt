a63d9835bf6342e3da155c1beb173ab5e908ffa4
fabiokusaba <fabinhokb44@gmail.com>
2024-09-05 21:57:41 -0300

    Added ResponseEntity to create author

--- src/main/kotlin/com/fabiokusaba/bookstore/controllers/AuthorsController.kt
+++ src/main/kotlin/com/fabiokusaba/bookstore/controllers/AuthorsController.kt
@@ -4,6 +4,8 @@ import com.fabiokusaba.bookstore.domain.dto.AuthorDto
 import com.fabiokusaba.bookstore.services.AuthorService
 import com.fabiokusaba.bookstore.toAuthorDto
 import com.fabiokusaba.bookstore.toAuthorEntity
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -12,7 +14,10 @@ import org.springframework.web.bind.annotation.RestController
 class AuthorsController(private val authorService: AuthorService) {
 
     @PostMapping(path = ["/v1/authors"])
-    fun createAuthor(@RequestBody authorDto: AuthorDto) : AuthorDto {
-        return authorService.save(authorDto.toAuthorEntity()).toAuthorDto()
+    fun createAuthor(@RequestBody authorDto: AuthorDto) : ResponseEntity<AuthorDto> {
+        val createdAuthor = authorService.save(
+            authorDto.toAuthorEntity()
+        ).toAuthorDto()
+        return ResponseEntity(createdAuthor, HttpStatus.CREATED)
     }
 }
