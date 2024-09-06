8d2ab000ae0102a0259e60f1565ff498200c5768
fabiokusaba <fabinhokb44@gmail.com>
2024-09-06 20:41:16 -0300

    Added testAuthorEntity

--- src/test/kotlin/com/fabiokusaba/bookstore/TestDataUtil.kt
+++ src/test/kotlin/com/fabiokusaba/bookstore/TestDataUtil.kt
@@ -1,6 +1,7 @@
 package com.fabiokusaba.bookstore
 
 import com.fabiokusaba.bookstore.domain.dto.AuthorDto
+import com.fabiokusaba.bookstore.domain.entities.AuthorEntity
 
 fun testAuthorDtoA(id: Long? = null) = AuthorDto(
     id = id,
@@ -9,3 +10,12 @@ fun testAuthorDtoA(id: Long? = null) = AuthorDto(
     description = "Some description",
     image = "author-image.jpeg"
 )
+
+fun testAuthorEntityA(id: Long? = null) = AuthorEntity(
+    id = id,
+    name = "John Doe",
+    age = 30,
+    description = "Some description",
+    image = "author-image.jpeg"
+)
+
