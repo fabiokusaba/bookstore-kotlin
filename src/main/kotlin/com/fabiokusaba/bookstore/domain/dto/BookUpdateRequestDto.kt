fc97474302d45059ba1739b4dbd449a6c9c62199
Fabio Kusaba <fabinhokb44@gmail.com>
2024-09-14 09:39:06 -0300

    Added null values in the properties

--- src/main/kotlin/com/fabiokusaba/bookstore/domain/dto/BookUpdateRequestDto.kt
+++ src/main/kotlin/com/fabiokusaba/bookstore/domain/dto/BookUpdateRequestDto.kt
@@ -1,8 +1,8 @@
 package com.fabiokusaba.bookstore.domain.dto
 
 data class BookUpdateRequestDto(
-    val title: String?,
-    val description: String?,
-    val image: String?
+    val title: String? = null,
+    val description: String? = null,
+    val image: String? = null
 ) {
 }
